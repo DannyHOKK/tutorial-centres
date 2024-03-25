5ce9d9bdf9be1760acb99721b2885f515b72bbbf
DannyHOKK <dannyhkk102@gmail.com>
2024-03-25 18:08:12 +0800

    updates

--- src/components/studentMatching/StudentInfo.jsx
+++ src/components/studentMatching/StudentInfo.jsx
@@ -5,6 +5,13 @@ const StudentInfo = ({ studentDetails }) => {
   return (
     <div>
       <div>學生編號：{studentDetails.id}</div>
+      <div>創建日期：{studentDetails?.createDate.split("T")[0]}</div>
+
+      <div className="student-info-list">
+        <div>中文姓名：{studentDetails.chineseName}</div>
+        <div>英文姓名：{studentDetails.engName}</div>
+        <div>學生編號：{studentDetails.id}</div>
+      </div>
     </div>
   );
 };
